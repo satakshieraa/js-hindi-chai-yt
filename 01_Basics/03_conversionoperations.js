97c37f2c01a46fdd644ba0ec95bb00dcfef3933e
satakshi raj <satakshieraa@gmail.com>
2024-12-20 22:11:22 +0530

    file add

--- 01_Basics/03_conversionoperations.js
+++ 01_Basics/03_conversionoperations.js
@@ -1,5 +1,5 @@
 let score = 33;
-console.log(typeof score);
+//console.log(typeof score);
 /*now somtimes we dont know that wether the value assigned to us 
 is a string or number for Eg*/
 
@@ -7,7 +7,7 @@ let score2 = "33" //will print a string
 //now to convert it in a vlaue
 
 let valueinnumber = Number(score2);
-console.log(typeof valueinnumber);
+//console.log(typeof valueinnumber);
 //output types is a number
 
 //now if score is like 33ab ,undefined , null or a boolean the 1 for true and 0 for false then the above
@@ -19,12 +19,40 @@ console.log(typeof valueinnumber);
 
 let isloggedin = "hitesh"
 let booleanisloggedin = Boolean(isloggedin)
-console.log(booleanisloggedin);
+//console.log(booleanisloggedin);
 
 let somenumber = 33;
 let stringnumber = String(somenumber);
-console.log(stringnumber);
-console.log(typeof stringnumber);
+//console.log(stringnumber);
+//console.log(typeof stringnumber);
  //it was a number i converted it in a string hahaha
  //How ? who cares :)
 
+ // *********** Operations *********** //
+ 
+ let value = 3;
+ let negvalue = -value;
+ //console.log(negvalue);
+ 
+ //console.log(2+2);
+ //console.log(2-2);
+ //console.log(2*2);  
+ //console.log(2/2);
+ //console.log(2**2);
+ ///console.log(2%2);
+
+ let str1 = "Hello";
+ let str2 = "satakshi"
+
+ let str3 = str1+str2;
+ console.log(str3)
+
+ //console.log("1" + 2);//12
+ //console.log(1 + "2");//12
+ //console.log("1" + 2 + 2);//122
+ //console.log(1 + 2 + "2");//32
+
+ //operator prercedence useless why is there no emoji
+
+ // prefix operator +1 and postfixoperator 1+
+ // read on mdn about prefix & postfix 
