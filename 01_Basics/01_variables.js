65aa2fddbd8551b040ad4660f07352e469e8eda2
satakshi raj <satakshieraa@gmail.com>
2024-12-20 19:29:24 +0530

    add

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,13 @@
+const account = 22548;
+let accemail = "ndhfhnksns";
+var opop = "jaipur";
+accoutncity = "kerela";
+let actcity  ;
+
+console.log(accemail);
+
+/* prefer not to use var
+because of issue in block scope and functional scope*/ 
+console.table([account, accemail, opop, accoutncity, actcity]);
+
+//in terminal write node 01_Basics/01_variable.js to see otuput
---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,25 @@
+let name = "satakshi";
+let age = 18;
+let isloggedin = false;
+let state = null;
+
+
+//number => 2 to 53
+//bigint
+//string = "" and ''
+//boolean = true || false
+//null is a stand alone value (representaion of an empty value)
+// undefined => when we have not defined the value abhi value asign nhi hue hai
+//symbol = when we make a lot of components and we have to identify
+// that this component is unique so to find that uniqeness symbol is very important
+
+// ^ above are primitive data types
+
+//object
+
+console.log(typeof undefined);
+console.log(typeof null);
+
+//undefined is a type in itself but null is a object
+
+
