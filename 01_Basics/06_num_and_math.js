ba3d9331a1fc8d16e58b6c3869fef3dc814d5304
satakshi raj <satakshieraa@gmail.com>
2024-12-21 17:47:37 +0530

    num and math

--- 01_Basics/06_num_and_math.js
+++ 01_Basics/06_num_and_math.js
@@ -11,12 +11,14 @@ const otherNumber = 123.8966 //123.9
 
 // console.log(otherNumber.toPrecision(4));
 //what precise means is to give the number closest value
-//jaisai precise value agar .toPrecision(3) mai agar 3 diye hai aur input 12.889 toh output ayaiga 12.8 like only 3 numbers as the input given in the precision was 3 if i had give (4) and input was 1256.9587, then output would have been 1257 to the nearest value and only 4 digits
+//jaisai precise value agar .toPrecision(3) mai agar 3 diye hai aur input 12.889 toh output ayaiga 12.8 like only 3 numbers 
+// as the input given in the precision was 3 if i had give (4) and input was 1256.9587, then output would have been 1257 to the nearest value and only 4 digits
 
 const hundreds = 1000000
 // console.log(hundreds.toLocaleString('en-IN')); 10,00,000
 
-// waisai default toh american currency hota  hai but agar indian curr mai change karna hai toh 'en-IN' daina parta hai then output will be 10,00,000 this will be the output 
+// waisai default toh american currency hota  hai but agar indian curr mai change karna hai toh 'en-IN' daina parta hai then output will 
+// be 10,00,000 this will be the output 
 
 
 // +++++++++++++ Maths +++++++++++++++++++++++++++++
@@ -34,7 +36,8 @@ console.log(Math.round(4.3));// simple round off
 console.log(Math.random()); // value always lies between 0 and 1 jaisai dice game mai value 1 sai 6 kai beech mai chaheyai
 console.log((Math.random()*10) + 1);
 console.log(Math.floor(Math.random()*10) + 1);// floor basically round of to lowest value
-console.log(Math.floor(Math.random()*100) +1);// jaisai *10 ka mtlb hai single digit value ayaigie and *100 ka mtlb hai 2 digit mai value ayaigie like 65 and 89 aisai
+console.log(Math.floor(Math.random()*100) +1);// jaisai *10 ka mtlb hai single digit value ayaigie and *100 ka mtlb hai 2
+//  digit mai value ayaigie like 65 and 89 aisai
 // and agar hum chatai hai kie value sabsai choti ayai toh hum use kartai hai math.floor okay !
 //floor basically round of kr deta hai lowest value pai
 // to avoid getting the value in 0 we add +1 in it 
