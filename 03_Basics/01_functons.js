5f92b86f20853a37f14ce487ad2a7ca2aa2d1ad8
satakshi raj <satakshieraa@gmail.com>
2024-12-22 01:57:27 +0530

    add functons

---
+++ 03_Basics/01_functons.js
@@ -0,0 +1,79 @@
+
+function sayMyName(){
+    console.log("H");
+    console.log("I");
+    console.log("T");
+    console.log("E");
+    console.log("S");
+    console.log("H");
+}
+
+// sayMyName() //to call the function to execute iska refrence
+
+
+// function addTwoNumbers(number1, number2){
+
+//     console.log(number1 + number2);
+// }
+
+function addTwoNumbers(number1, number2){
+
+    // let result = number1 + number2
+    // return result
+    return number1 + number2
+}
+
+const result = addTwoNumbers(3, 5)
+
+// console.log("Result: ", result);
+
+
+function loginUserMessage(username = "sam"){
+    if(!username){
+        console.log("PLease enter a username");
+        return
+    }
+    return `${username} just logged in`//output undefined
+}
+
+//console.log(loginUserMessage()); //undefined output
+
+// jab kuch pass nhi kartai hai toh wo aata hai undefined
+// console.log(loginUserMessage("hitesh"))
+// console.log(loginUserMessage("hitesh"))
+
+
+function calculateCartPrice(val1, val2, ...num1){ 
+    return num1
+}
+
+// console.log(calculateCartPrice(200, 400, 500, 2000)) ,,,,,,,,,,,num1 value will be [500, 2000]
+
+/*we have a rest operator ... yeah ! same but use cases are diff
+now mtlb kie jitna bhi item aapko abhi mila hai woh bikhra hua hai 
+toh in sabko ek bundle mai pack karo aur mujhe dai do toh yeh array kay andar add hotai jatai hai*/
+
+
+const user = {       // yeh object ko F mai kaissai pass and use kiya jata hai
+    username: "hitesh",
+    prices: 199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// handleObject(user)
+handleObject({
+    username: "sam",
+    price: 399
+})
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 500, 1000]));
