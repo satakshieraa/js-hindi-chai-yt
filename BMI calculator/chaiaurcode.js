d9370ccd8da164e7887aa805081efbcd1ed7a359
satakshi raj <satakshieraa@gmail.com>
2025-01-06 14:20:29 +0530

    commit

---
+++ BMI calculator/chaiaurcode.js
@@ -0,0 +1,40 @@
+const form = document.querySelector('form');
+
+form.addEventListener('submit', function (e){
+    e.preventDefault();
+
+    const height = parseInt(document.querySelector('#height').value);
+    const weight = parseInt(document.querySelector('#weight').value);
+    const results = document.querySelector('#results');
+
+    if(height === '' || height < 0 || isNaN(height)){
+    results.innerHTML = `please give  a valid ${height}`;
+    } 
+    
+    else if (weight === '' || weight < 0 || isNaN(weight)) {
+    results.innerHTML = `please give  a valid ${weight}`;
+    }
+
+    else {
+        const bmi = (weight / ((height*height) / 10000)).toFixed(2);
+        // show the result
+        results.innerHTML = `<span> ${bmi} </span>`
+    }
+
+})
+    
+    
+   /*
+   
+   if (bmi < 18.6){
+    console.log("under weight");
+    
+   } else if (bmi <= 18.6 || bmi >= 24.9) {
+    console.log("noremal weight");
+    
+   } else ( bmi > 24.9) {
+    console.log("overweight");
+    
+   }
+
+*/
