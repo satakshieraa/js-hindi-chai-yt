a9f55362386299e9a92d2320ff6c03bdc173073c
satakshi raj <satakshieraa@gmail.com>
2025-01-07 17:53:03 +0530

    commit

---
+++ UNLIMITED colors/chaiaurcode.js
@@ -0,0 +1,29 @@
+// genrate a random color
+
+
+const randomcolor = function(){
+    const hex = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0 ; i < 6 ; i++){
+    color += hex[Math.floor(Math.random() * 16)]; 
+    }
+    return color ;
+}
+let intervalid ;
+const satrtchangingcolor = function(){
+    if(!intervalid){
+     intervalid = setInterval(changebgcolor , 1000)
+    }
+    function changebgcolor(){
+        document.body.style.backgroundColor = randomcolor()
+
+    }
+};
+const stopchangingcolor = function(){
+    clearInterval(intervalid);
+    intervalid = null 
+
+};
+
+document.querySelector('#start').addEventListener('click', satrtchangingcolor);
+document.querySelector('#stop').addEventListener('click', stopchangingcolor);
